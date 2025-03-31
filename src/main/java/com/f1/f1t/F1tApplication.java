b874d7efea9662fae06759433254bf924f1fa12e
micha <michal.tusz12@gmail.com>
2025-03-31 16:58:31 +0200

    initial commit

---
+++ src/main/java/com/f1/f1t/F1tApplication.java
@@ -0,0 +1,13 @@
+package com.f1.f1t;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F1tApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(F1tApplication.class, args);
+	}
+
+}
