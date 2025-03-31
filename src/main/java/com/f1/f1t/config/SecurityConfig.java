bdba3f7269ddcf1bf9bc21a1ce422f9964ddcb91
micha <michal.tusz12@gmail.com>
2025-03-31 18:28:11 +0200

    request matchers change

--- src/main/java/com/f1/f1t/config/SecurityConfig.java
+++ src/main/java/com/f1/f1t/config/SecurityConfig.java
@@ -50,9 +50,8 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((requests) -> requests
-                .requestMatchers("/h2-console/**").permitAll()
                 .requestMatchers("/api/auth/**").permitAll()
-                .requestMatchers("/api/games/all").permitAll()
+                .requestMatchers("/api/unauthorized/**").permitAll()
                 .anyRequest().authenticated()
         ).oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
         http.csrf(AbstractHttpConfigurer::disable)
